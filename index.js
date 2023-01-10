82ab9e8862bc2138eefe42eec36f40a7c2b9b067
Vitoria-m <vitoriazzz@outlook.com>
2023-01-10 18:19:30 -0300

    exibindo lista suspensa case o checkbox for marcado

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const comCarencia = document.querySelector('#comCarencia')
+const listaSuspensa = document.querySelector('#listaSuspensa')
 
+comCarencia.addEventListener('change', function() {
+    if(this.checked) {
+        listaSuspensa.removeAttribute('hidden')
+    } 
+    else {
+        listaSuspensa.setAttribute('hidden', 'hidden')
+    }
+})
 
